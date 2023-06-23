e4a0893d111f86f9c3c43a9df995e4f8bdddaeff
General Pc <qwolil2110@gmail.com>
2023-06-23 11:48:12 +0300

    Initial commit

---
+++ src/main/java/com/example/messanger/controller/ChatController.java
@@ -0,0 +1,28 @@
+package com.example.messanger.controller;
+
+import com.example.messanger.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+
+import java.util.Objects;
+
+@Controller
+public class ChatController {
+
+    @MessageMapping("/chat.sendMessage")
+    @SendTo("/topic/public")
+    public ChatMessage sendMessage(@Payload ChatMessage message){
+        return message;
+    }
+
+    @MessageMapping("/chat.addUser")
+    @SendTo("/topic/public")
+    public ChatMessage addUser(@Payload ChatMessage message,
+                               SimpMessageHeaderAccessor accessor){
+        Objects.requireNonNull(accessor.getSessionAttributes()).put("username", message.getSender());
+        return message;
+    }
+}
